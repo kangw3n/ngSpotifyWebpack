baca7adaea815788176d80d60b134f54ef1e3088
kangw3n <weitochearyeah@hotmail.com>
2016-09-05 13:02:44 +0800

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,31 @@
+// import { Routes, RouterModule } from '@angular/router';
+// import { Home } from './home';
+// import { About } from './about';
+// import { NoContent } from './no-content';
+
+// import { DataResolver } from './app.resolver';
+
+
+// export const ROUTES: Routes = [
+//   { path: '',      component: Home },
+//   { path: 'home',  component: Home },
+//   { path: 'about', component: About },
+//   {
+//     path: 'detail', loadChildren: () => System.import('./+detail')
+//   },
+//   { path: '**',    component: NoContent },
+// ];
+
+
+import {Routes, RouterModule} from "@angular/router";
+import {SearchComponent} from "./components/search/search.component";
+import {AboutComponent} from "./components/about/about.component";
+import {ArtistComponent} from "./components/artist/artist.component";
+import {AlbumComponent} from "./components/album/album.component";
+
+export const ROUTES: Routes = [
+    {path: '', component: SearchComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'artist/:id', component: ArtistComponent},
+    {path: 'album/:id', component: AlbumComponent}
+];
